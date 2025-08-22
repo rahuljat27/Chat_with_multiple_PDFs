cb1888b84ea7d9855c1870bb7135e34c50c7e7c7
rahuljat27 <rahuljat13290@gmail.com>
2025-08-22 14:26:06 +0530

    fix: corrected user avatar URL

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://ibb.co/wZXy8XbP">
+        <img src="https://i.ibb.co/cKHJRHpZ/Rahul-Jat-Photo.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>

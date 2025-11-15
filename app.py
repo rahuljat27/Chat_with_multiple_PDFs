06565a285af55c76386444ff01ba1cf519d7d93a
rahuljat27 <151195785+rahuljat27@users.noreply.github.com>
2025-11-15 11:28:28 +0530

    few updates on langchain

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_history_aware_retriever
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from htmlTemplates import css, bot_template, user_template
 from langchain_groq import ChatGroq
 
